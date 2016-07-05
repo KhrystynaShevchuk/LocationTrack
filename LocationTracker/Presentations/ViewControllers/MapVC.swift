97dbf65ae443d499918959557f8afc024b9b2371
Khrystyna Shevchuk <khrystyna.shevchuk94@gmail.com>
2016-07-05 13:47:04 +0300

    - fixed extension-methods - separate setups from viewDidLoad - added tapGesture on popup - alpha on textView - refactor

--- LocationTracker/Presentations/ViewControllers/MapVC.swift
+++ LocationTracker/Presentations/ViewControllers/MapVC.swift
@@ -11,21 +11,78 @@ import MapKit
 import CoreLocation
 
 class MapVC: UIViewController {
+    
+    // MARK: - OUTLETS
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - PROPERTIES
     
     var popupView = ShowAddressCustomView()
     let screenSize = UIScreen.mainScreen().bounds
     var yPosition : CGFloat = 0
-    
     let locationManager = CLLocationManager()
     var currentAddress: String = ""
+    
+    // MARK: - LIFI CIRCLE
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupLocation()
+        setTapGestureOnPinLocation()
+        setupPopview()
+
+        // setup mapView
+        mapView.delegate = self
+        mapView.showsUserLocation = true
         
+        self.view.addSubview(popupView)
+        self.popupView.frame = CGRect(x: 0, y: screenSize.height, width: self.screenSize.width, height: 100)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
+        checkLocationAuthorizationStatus()
+    }
+    
+    // MARK: - PUBLIC
+    
+    func setupLocation() {
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func setTapGestureOnPinLocation() {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(MapVC.handleTap(_:)))
+        gestureRecognizer.delegate = self
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func setTapGestureOnPopup() {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(MapVC.tapPopupToHide(_:)))
+        gestureRecognizer.delegate = self
+        popupView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func handleTap(gestureReconizer: UILongPressGestureRecognizer) {
+        _ = gestureReconizer.locationInView(mapView)
+        if locationManager.location != nil {
+            makePopupViewVisible(true)
+        } else {
+            makePopupViewVisible(false)
+        }
+    }
+    
+    func tapPopupToHide(gestureReconizer: UILongPressGestureRecognizer) {
+        _ = gestureReconizer.locationInView(popupView)
+        makePopupViewVisible(false)
+    }
+    
+    func setupPopview() {
         if let views = NSBundle.mainBundle().loadNibNamed("ShowAddressCustomView", owner: nil, options: nil)
         {
             popupView = views.last as! ShowAddressCustomView
@@ -37,123 +94,91 @@ class MapVC: UIViewController {
             
             self.view.addSubview(popupView)
         }
-
-        // setup mapView
-        
-        
-        mapView.delegate = self
-        mapView.showsUserLocation = true
-        
-        // setup location
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        // gesture recognizer
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(MapVC.handleTap(_:)))
-        gestureRecognizer.delegate = self
-        mapView.addGestureRecognizer(gestureRecognizer)
-        
-       //popupView.frame = CGRectMake(0, 0, 100, 100)
-        
-        self.view.addSubview(popupView)
-    
-        self.popupView.frame = CGRect(x: 0, y: screenSize.height, width: self.screenSize.width, height: 100)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        checkLocationAuthorizationStatus()
     }
     
     func checkLocationAuthorizationStatus() {
         if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
             mapView.showsUserLocation = true
         } else {
-            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestAlwaysAuthorization()
         }
     }
     
-    func handleTap(gestureReconizer: UILongPressGestureRecognizer) {
-        _ = gestureReconizer.locationInView(mapView)
-        makePopupViewVisible(true)
-    }
-
+    // MARK: - PRIVATE
     
+    // set popup, popup visibility
     private func makePopupViewVisible(isViewVisible: Bool) {
         if isViewVisible == true {
             self.popupView.addressTextView.text = self.currentAddress
-            
             yPosition = screenSize.height - 110
-            setShowAddressCustomView(yPosition)
+            setPopupOnView(yPosition)
+            setTapGestureOnPopup()
             
         } else {
             yPosition = screenSize.height
-            setShowAddressCustomView(yPosition)
+            setPopupOnView(yPosition)
+            setTapGestureOnPinLocation()
         }
     }
     
-    private func setShowAddressCustomView(yPosition: CGFloat) {
+    private func setPopupOnView(yPosition: CGFloat) {
         UIView.animateWithDuration(0.7, delay: 1.0, options: .CurveEaseOut, animations: {
             self.popupView.frame = CGRect(x: 0, y: yPosition, width: self.screenSize.width, height: 100)
             }) { finished in
         }
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    // get address by coordinates
+    private func getAddressByCoordinates(location: CLLocation) {
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+            self.currentAddress = ""
+            
+            // Place details
+            var placeMark: CLPlacemark?
+            placeMark = placemarks?[0]
+            
+            // Address dictionary
+            if placeMark != nil {
+                print(placeMark!.addressDictionary)
+            }
+            
+            // Street address
+            if let street = placeMark?.addressDictionary!["Thoroughfare"] as? NSString {
+                let streetValue = "STREET :  \(street)\n"
+                self.currentAddress = self.currentAddress + streetValue
+            }
+            
+            // City
+            if let city = placeMark?.addressDictionary!["City"] as? NSString {
+                let cityValue = "CITY :    \(city)\n"
+                self.currentAddress = self.currentAddress + cityValue
+            }
+            
+            // Country
+            if let country = placeMark?.addressDictionary!["Country"] as? NSString {
+                let countryValue = "COUNTRY :  \(country)\n"
+                self.currentAddress = self.currentAddress + countryValue
+            }
+            
+            // Country code
+            if let countryCode = placeMark?.addressDictionary!["CountryCode"] as? String {
+                let countryCodeValue = "COUNTRY CODE :  \(countryCode)"
+                self.currentAddress = self.currentAddress + countryCodeValue
+            }
+        })
     }
 }
 
 extension MapVC: UIGestureRecognizerDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
     
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locationManager.location!
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let span = MKCoordinateSpan(latitudeDelta: 0.08, longitudeDelta: 0.08)
         let region = MKCoordinateRegion(center: center, span: span)
         
         mapView.setRegion(region, animated: true)
-        
-        let geoCoder = CLGeocoder()
-        geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
-            
-            self.currentAddress = ""
-                        
-            // Place details
-            var placeMark: CLPlacemark!
-            placeMark = placemarks?[0]
-            
-            // Address dictionary
-            print(placeMark.addressDictionary)
-            
-            // Street address
-            if let street = placeMark.addressDictionary!["Thoroughfare"] as? NSString {
-                let streetValue = "STREET :  \(street)\n"
-                self.currentAddress = self.currentAddress + streetValue
-            }
-            
-            // City
-            if let city = placeMark.addressDictionary!["City"] as? NSString {
-                let cityValue = "CITY :    \(city)\n"
-                self.currentAddress = self.currentAddress + cityValue
-            }
-            
-            
-            // Country
-            if let country = placeMark.addressDictionary!["Country"] as? NSString {
-                let countryValue = "COUNTRY :  \(country)\n"
-                self.currentAddress = self.currentAddress + countryValue
-            }
-            
-            // Country code
-            if let countryCode = placeMark.addressDictionary!["CountryCode"] as? String {
-                let countryCodeValue = "COUNTRY CODE :  \(countryCode)"
-                self.currentAddress = self.currentAddress + countryCodeValue
-            }
-        })
+        getAddressByCoordinates(location)
     }
 }
